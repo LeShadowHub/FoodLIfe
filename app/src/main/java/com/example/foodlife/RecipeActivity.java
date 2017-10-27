72d650d228fe1c44ec8c0f9621f7958944da8b10
SirQwertyMan <kevin.lee979@utexas.edu>
2017-10-27 04:32:51 -0500

    Floating button for adding recipes added

--- app/src/main/java/com/example/foodlife/RecipeActivity.java
+++ app/src/main/java/com/example/foodlife/RecipeActivity.java
@@ -1,7 +1,9 @@
 package com.example.foodlife;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +21,16 @@ public class RecipeActivity extends AppCompatActivity implements NavigationView.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
+        //Floating add button- opens new activity
+        FloatingActionButton newRecipe = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        newRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addRecipe(view);
+            }
+        });
+
+        //navigation menu
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
@@ -52,5 +64,12 @@ public class RecipeActivity extends AppCompatActivity implements NavigationView.
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         return mToggle.onOptionsItemSelected(item)|| super.onOptionsItemSelected(item);
+    }
+
+    public void addRecipe(View view){
+        Snackbar.make(view, "Add Recipe", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+        Intent intent = new Intent(this, addRecipes.class);
+        startActivity(intent);
     }
 }
