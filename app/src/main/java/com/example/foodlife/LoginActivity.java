a1084a7b51a0e395e7a36348fe5baa63552fa116
SirQwertyMan <kevin.lee979@utexas.edu>
2017-10-27 04:24:34 -0500

    Navigation Menu added to Recipe Section

--- app/src/main/java/com/example/foodlife/LoginActivity.java
+++ app/src/main/java/com/example/foodlife/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.foodlife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Intent intent = new Intent(this, RecipeActivity.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/foodlife/RecipeActivity.java
+++ app/src/main/java/com/example/foodlife/RecipeActivity.java
@@ -1,13 +1,56 @@
 package com.example.foodlife;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(this);
+        }
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onNavigationItemSelected(MenuItem item){
+
+        int id = item.getItemId();
+        if(id == R.id.nav_Groceries){
+            Intent intent = new Intent(this, GroceryActivity.class);
+            startActivity(intent);
+        }
+        if(id == R.id.nav_Pantry){
+            Intent intent = new Intent(this, PantryActivity.class);
+            startActivity(intent);
+        }
+        if(id == R.id.nav_Recipes){
+
+        }
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        return mToggle.onOptionsItemSelected(item)|| super.onOptionsItemSelected(item);
     }
 }
